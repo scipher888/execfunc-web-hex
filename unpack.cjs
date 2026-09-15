aa0327579be0d85fb7068c3ae40298aa75b08669
scipher888 <scipher800@gmail.com>
2026-09-15 14:29:07 -0700

    Update unpack.cjs for form-POST Home/Inbox hex pack

--- unpack.cjs
+++ unpack.cjs
@@ -2,6 +2,6 @@ const { readFileSync, writeFileSync } = require("fs");
 const { execSync } = require("child_process");
 const hex = ["h1","h2","h3","h4","h5","h6"].map((f) => readFileSync(f + ".txt", "utf8").replace(/\s+/g, "")).join("");
 const buf = Buffer.from(hex, "hex");
-if (buf.length !== 19166) throw new Error("bad tgz length " + buf.length);
+if (buf.length !== 18493) throw new Error("bad tgz length " + buf.length);
 writeFileSync("slice.tgz", buf);
 execSync("tar -xzf slice.tgz");
